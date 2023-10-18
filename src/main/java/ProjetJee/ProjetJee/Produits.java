209fae3440511f0b37827e1090ff6d7d618ec5c3
adri6400 <bernardadr@cy-tech.fr>
2023-10-18 12:16:07 +0200

    BDD

---
+++ src/main/java/ProjetJee/ProjetJee/Produits.java
@@ -0,0 +1,74 @@
+package ProjetJee.ProjetJee;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Produits {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private double prix;
+	private int stock;
+	private String numeroPlace;
+	@ManyToOne
+    @JoinColumn(name = "idCategories")
+    private Categorie categorie;
+	public Categorie getCategorie() {
+        return categorie;
+    }
+	public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getNumeroPlace() {
+		return numeroPlace;
+	}
+	public void setNumeroPlace(String numeroPlace) {
+		this.numeroPlace = numeroPlace;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(categorie, id, name, numeroPlace, prix, stock);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produits other = (Produits) obj;
+		return Objects.equals(categorie, other.categorie) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name) && Objects.equals(numeroPlace, other.numeroPlace)
+				&& Double.doubleToLongBits(prix) == Double.doubleToLongBits(other.prix) && stock == other.stock;
+	}
+	
+	
+}
