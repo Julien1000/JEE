81e95e3391c0c8dd5d1703e595e64aec27b2502f
Julien1000 <papinijuli@cy-tech.fr>
2023-10-18 10:41:09 +0200

    Premier commit : Ajout de fichiers initiaux

---
+++ ProjetJee/src/test/java/ProjetJee/ProjetJee/ProjetJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetJee.ProjetJee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
