d71c93333d5937e8a2dbb5bb434708371bcbcdde
Hippopo64 <103028035+Hippopo64@users.noreply.github.com>
2023-10-20 10:39:42 +0200

    ajout de fonctionnalités js

--- src/main/resources/templates/navbar.js
+++ src/main/resources/templates/navbar.js
@@ -6,5 +6,13 @@ window.onload = function() {
       profileMenu.classList.toggle('hidden');
     });
 
-    
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    mobileMenuButton.addEventListener('click', function() {
+      mobileMenu.classList.toggle('hidden');
+    });
+
+
+
   };
