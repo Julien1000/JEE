8c77d5634683cd7d2cf329b3e430a8cd099fb748
Hippopo64 <103028035+Hippopo64@users.noreply.github.com>
2023-10-27 10:46:05 +0200

    yes

--- src/main/resources/static/js/produitForm.js
+++ src/main/resources/static/js/produitForm.js
@@ -1,33 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const listbox = document.querySelector(".list-category");
-  const button = document.querySelector(".button_select_category");
+//   const listbox = document.querySelector(".list-category");
+//   const button = document.querySelector(".button_select_category");
 
-  button.addEventListener("click", function () {
-    if (listbox.style.display === "block") {
-      listbox.style.display = "none";
-    } else {
-      listbox.style.display = "block";
-    }
-  });
+//   button.addEventListener("click", function () {
+//     if (listbox.style.display === "block") {
+//       listbox.style.display = "none";
+//     } else {
+//       listbox.style.display = "block";
+//     }
+//   });
 
-  const list = document.querySelectorAll(".list-category li");
-  list.forEach(function (li) {
-    li.addEventListener("click", function () {
-      list.forEach(function (li) {
-        li.querySelector("svg").classList.remove("svg_selected");
-        li.querySelector("svg").classList.add("svg_select");
-      });
+  // const list = document.querySelectorAll(".list-category li");
+  // list.forEach(function (li) {
+  //   li.addEventListener("click", function () {
+  //     list.forEach(function (li) {
+  //       li.querySelector("svg").classList.remove("svg_selected");
+  //       li.querySelector("svg").classList.add("svg_select");
+  //     });
 
-      const category = document.querySelector(".category_choosen");
-      li_value = li.querySelector(".category_value").textContent;
-      category.textContent = li_value;
-      listbox.style.display = "none";
+  //     const category = document.querySelector(".category_choosen");
+  //     li_value = li.querySelector(".category_value").textContent;
+  //     category.textContent = li_value;
+  //     listbox.style.display = "none";
 
-      const svg = li.querySelector("svg");
-      svg.classList.remove("svg_select");
-      svg.classList.add("svg_selected");
-    });
-  });
+  //     const svg = li.querySelector("svg");
+  //     svg.classList.remove("svg_select");
+  //     svg.classList.add("svg_selected");
+  //   });
+  // });
 
   const fileInput = document.querySelector(".input_file");
   const fileUploadDiv = document.querySelector(".input_file_area");
